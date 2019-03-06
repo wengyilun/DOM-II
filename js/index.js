701eef0e150adaad18259b6307d314d0703f0134
Ellen Weng <wengellen@gmail.com>
2019-03-05 18:56:46 -0800

    Finishes MVP

--- js/index.js
+++ js/index.js
@@ -1,33 +1,49 @@
 // Your code goes here
-let message = document.getElementById('message')
-console.log('message', message)
+
 let anchorLinks = document.querySelectorAll('.nav-link')
 anchorLinks.forEach((link, currentIndex) => {
+    // 1. Mouseover Event
+    // Prevent Default
     link.addEventListener('mouseover', function(e){
         e.preventDefault();
         e.target.classList.add('scale')
         e.target.style.transform = `translateX(10px)`
         e.target.style.transition = `all 0.5s ease`
     })
+    
+     // 2. Mouseout Event
     link.addEventListener('mouseout', function(e){
         e.target.classList.remove('scale')
         e.target.style.transform = `translateX(-10px)`
         e.target.style.transition = `all 0.5s ease`
     })
 
+     // 3. Double Click Event
+    link.addEventListener('dblclick', function(e){
+        e.target.style.backgroundColor = `red`
+        e.target.style.transition = `all 0.5s ease`
+    })
     
-
   } , 'thisArg'
 )
 
 let contentSections = document.querySelectorAll('.content-section')
 contentSections.forEach((section) => {
+    // Prevent propagation
+    section.addEventListener('dragstart', function(e){
+        console.log('Should not see this ::: parent constainer dragstart captured')
+    })
+
     let children = Array.from(section.children)
     children.forEach(item => {
         item.draggable = true;
+        // 4. Drags/Drop 
+        // Prevent propagation
         item.addEventListener('dragstart', function(e){
+            e.stopPropagation();
             console.log('item', item)
-            item.style.transition = `all 1s ease`;
+            item.style.border = "1px solid red"
+            console.log('child element dragstart captured')
         })
 
         item.addEventListener('dragend', function(e){
@@ -36,16 +52,13 @@ contentSections.forEach((section) => {
              }else{
                 item.style.order = item.style.order * -1;
              }
-        })
-
-        item.addEventListener('focus', function(e){
-            item.style.border = '1px dash blue'
-            item.style.color = 'red'
-            //message.textContent = item.textContent
+             item.style.border = "none"
         })
     })
 })
 
+
+// 5. Wheel Event
 let body = document.getElementsByTagName("body")[0]
 body.addEventListener('wheel', function(e){
     anchorLinks.forEach((link, currentIndex) => {
@@ -58,13 +71,42 @@ body.addEventListener('wheel', function(e){
     })
 })
 
-
+// 6. Resize Event
 function handleResize(){
     console.log(`Window is being resized to ${document.body.clientWidth}px width and ${document.body.clientHeight}px height`);
 }
 
-/*
-message.addEventListener('keydown', function(e){
-    e.target.classList.remove('scale')
+let message = document.getElementById('message')
+console.log('message', message)
+
+let messageInput = message.getElementsByTagName('input')
+console.log('messageInput', messageInput)
+
+// 7. Focus Event
+inputmessage.addEventListener('focus', function(e){
+    e.target.style.borderColor = 'aliceblue';
+    e.target.style.backgroundColor = 'aliceblue';
 })
-*/
+
+// 8. Keydown Event
+inputmessage.addEventListener('keydown', function(e){
+    console.log(e)
+    let str = 'aeiou'
+    e.target.style.transition = "all 0.3s ease"
+    if(str.includes(e.key)){
+        e.target.style.fontSize = "40px";
+        e.target.style.color = 'red';
+    }else{
+        e.target.style.fontSize = "initial";
+        e.target.style.color = 'initial';
+    }
+})
+
+// 9.Load Event
+window.addEventListener('load', function(e){
+    document.getElementById('inputmessage').focus();
+})
+// 10.Scroll Event
+window.addEventListener('scroll', function(e){
+    document.getElementById('message').style.backgroundColor = "red";
+})
